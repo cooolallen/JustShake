f417e3e733be558554c96bbe80746f463f6bbfa1
Rajith Weerasinghe <rweerasi@purdue.edu>
2017-09-24 07:41:12 -0400

    Add Server Files

---
+++ Logger.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 24 05:26:22 2017
+
+@author: Rajith
+"""
+
+from enum import Enum
+from time import time
+
+class Singleton(type):
+    _instances = {}
+    def __call__(cls,*args,**kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args,**kwargs)
+        return cls._instances[cls]
+
+class GameState(Enum):
+    Waiting = 0
+    Playing = 1
+    Done = 2
+
+class Logger(metaclass=Singleton):
+    def __init__(self):
+        self.startTime = time()     
+        self.players = {}  
+        self.state = GameState.Waiting;
+    
+    def setState(gameState):
+        if gameState in GameState.__members__:
+            self.state = gameState
+        
+    def playerJoin(self,userID):
+        # Initalize score to zero
+        self.players.update({userID : 0})
+    
+    def updateScores(self,userID,points):
+        # TODO: Timestamp Logic
+        self.players[userID] += points
+        
+    def report(self):
+        return self.players
---
+++ Server.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 22 22:42:13 2017
+
+@author: Rajith
+"""
+
+from flask_socketio import SocketIO, emit
+from flask import Flask, request
+import json, csv
+import Logger
+    
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+print("Starting Server")
+l = Logger.Logger()
+choreography = {}
+timeList = [100, 120, 130]
+'''
+with open('timetable.csv','rb) as file:
+    reader = csv.reader(file, delimiter="\t")
+    for row in reader:
+       data = row.split('\t) 
+       choreography.update({ data[0] : data[1] })
+       timeList.append(data[0])
+'''
+
+@app.route('/',methods=["GET"])
+def postInfo():
+    if request.method is "GET":
+        return "Hello World"
+    json = request.get_json(silent=True)
+    if 'data' in json:
+        '''print(json)'''
+        username = json['userID']
+        data = json['data']
+        timestamp = json['time']
+        l.updateScores(username, data, timestamp)
+        return "Action recieved"
+    elif 'userID' in json:
+        l.playerJoin(json['userID'])
+        if len(l.players) == 3:
+            l.state = Logger.GameState.Playing
+        return "User added"
+ 
+@app.route('/gamestatus/',methods=["GET"])
+def gameStatus():
+    '''
+    Check whether the game is running.
+    
+    :param: none
+    :returns: string describing the status of the game
+    '''        
+    if l.state is Logger.GameState.Playing:
+        return "GAME WAIT"
+    if l is None:
+        return "GAME PLAYING"
+    return "GAME OVER"
+
+@app.route('/action/',methods=["POST"])
+def addAction():
+    '''print(request.get_json(silent=True))'''
+    json = request.get_json(silent=True)
+    username = json["userID"]
+    if "time" not in json:
+        l.playerJoin(json['userID'])
+        if len(l.players) == 3:
+            l.state = Logger.GameState.Playing
+        return ""
+    timestamp = float(json["time"])
+    diff = abs(timestamp - min(timeList, key=lambda x:abs(x-timestamp)))
+    if diff < 0.1:
+        l.updateScore(username,100)
+    elif diff < 0.5:
+        l.updateScore(username,20)
+    return "Action Recieved"
+
+@app.route("/score/", methods=["GET"])
+def getScoreData():
+    '''
+    Show current scores
+    :params: None
+    :returns: JSON representation of players and their scores
+    '''
+    if l is None:
+        return ""
+    return json.dumps(l.players);
+'''
+@app.route("/socket.io/")
+def asdf():
+    if request.method is "GET":
+        return ""
+    json = request.get_json(silent=True)
+    print(json)
+    return ""
+   ''' 
+# For debugging
+@app.route("/init/",methods=["GET"])
+def startLogger():  
+    return "Initialization Successful"
+# For debugging    
+@app.route('/data/<string:username>/<string:data>/<string:timestamp>')
+def specifyAction(username,data,timestamp):
+    l.updateScores(username,data,timestamp)
+    return "Recieved"
+# For debugging
+@app.route("/join/<string:username>/", methods=["GET"])
+def playerJoin(username):
+    if l is None:
+        return "Create a new game"
+    l.playerJoin(username)
+    if len(l.players) == 3:
+        l.state = Logger.GameState.Playing
+    return "Joined"
+
+@socketio.on('start')
+def reply():
+    emit('Response')
